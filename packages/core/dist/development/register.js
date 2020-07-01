fd99e597923e0310aff45115194bc4de69230891
matsuo-ts <matsuo@beecruise.co.jp>
2020-07-01 15:27:26 +0900

    add dist

---
+++ packages/core/dist/development/register.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var register = require('./register-9eb87bae.js');
+require('path');
+require('fs-extra');
+require('react');
+require('slash');
+require('lz-string');
+require('urlsafe-base64');
+require('fs');
+require('recursive-readdir');
+
+
+
+module.exports = register.register;
---
+++ packages/core/dist/production/register.js
@@ -0,0 +1,1 @@
+"use strict";var e=require("./register-9eb87bae.js");require("path"),require("fs-extra"),require("react"),require("slash"),require("lz-string"),require("urlsafe-base64"),require("fs"),require("recursive-readdir"),module.exports=e.register;
